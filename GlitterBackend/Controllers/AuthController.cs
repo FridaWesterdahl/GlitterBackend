a379f8d9e6cd9a25ff62987353cf0f970a7121eb
Frida <fridaawesterdahl@gmail.com>
2022-05-24 23:12:41 +0200

    Added auth, verify password left to fix.

---
+++ GlitterBackend/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GlitterBackend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using BCrypt.Net;
+
+namespace GlitterBackend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly EFContext _EFContext;
+        public AuthController(IConfiguration configuration, EFContext context)
+        {
+            _configuration = configuration;
+            _EFContext = context;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] AuthUser authUser)
+        {
+            var resp = Authenticate(authUser);
+
+            if (resp != null)
+            {
+                var token = Generate(resp);
+                return Ok(token);
+            }
+
+            return NotFound("User not found");
+        }
+
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"], claims, expires: DateTime.Now.AddDays(7),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User Authenticate(AuthUser authUser)
+        {
+            
+            var hashedPassword = user.Password;
+            var verifiedPassword = BCrypt.Net.BCrypt.Verify(hashedPassword, authUser.Password);
+
+            var resp = _EFContext.Users.FirstOrDefault(x => x.Username.ToLower() == 
+            authUser.Username.ToLower() && verifiedPassword);
+
+            if (resp != null)
+            {
+                return resp;
+            }
+
+            return null;
+        }
+    }
+}
--- GlitterBackend/Program.cs
+++ GlitterBackend/Program.cs
@@ -1,6 +1,9 @@
+using System.Text;
 using GlitterBackend;
 using GlitterBackend.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +13,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<EFContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("Connectionstring")));
-
-
-
+builder.Services.AddDbContext<EFContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("Connectionstring")));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+builder.Services.AddMvc();
 
 
 var app = builder.Build();
@@ -30,11 +44,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
-
-
 
 app.UseCors(x => x
     .AllowAnyOrigin()
